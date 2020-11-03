e84c3e31ca41bfc5486b30567340c33763093330
hiroki tamura <hiirookii0625@gmail.com>
2020-11-03 21:49:28 +0900

    blog 投稿機能実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,26 @@
 class BlogsController < ApplicationController
+
   def index
     @blogs = Blog.all
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "投稿しました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
 end
