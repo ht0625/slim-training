26ad2b97c05de6d61179796741e1ab8f44bf4bac
hiroki tamura <hiirookii0625@gmail.com>
2020-11-19 23:25:04 +0900

    sessionコントローラー生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'blogs#index'
   resources :blogs
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
