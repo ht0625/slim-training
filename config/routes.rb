ada4ac6e59446a933ae150ba41894b6b5b4cfdb9
hiroki tamura <hiirookii0625@gmail.com>
2020-11-29 23:40:16 +0900

    root変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'tops#index'
   get 'tops/index'
-  root 'blogs#index'
   resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
